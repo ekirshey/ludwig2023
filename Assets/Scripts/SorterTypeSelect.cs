d1357da2e6871084a4f4f07c79927536f103f019
ekirshey <ekirshey@gmail.com>
2023-02-22 11:28:12 -0500

    Add sorter button

--- Assets/Scripts/SorterTypeSelect.cs
+++ Assets/Scripts/SorterTypeSelect.cs
@@ -7,28 +7,50 @@ namespace SadBrains
 {
     public class SorterTypeSelect : MonoBehaviour
     {
+        [SerializeField] private Sprite baseSprite;
+        [SerializeField] private Sprite onClickSprite;
+        [SerializeField] private GameObject bottomUp;
+        [SerializeField] private SpriteRenderer bottomUpHeadshot;
+        [SerializeField] private GameObject bottomDown;
+        [SerializeField] private SpriteRenderer bottomDownHeadshot;
         public event Action<CootsType> CootsTypeChanged;
 
-        private SpriteRenderer _spriteRenderer;
+        private SpriteRenderer _buttonRenderer;
         private BoxCollider2D _boxCollider2D;
         private List<CootsType> _cootsTypes;
         private int _typeIdx;
 
         private void Start()
         {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _buttonRenderer = GetComponent<SpriteRenderer>();
             _boxCollider2D = GetComponent<BoxCollider2D>();
             _cootsTypes = new List<CootsType>();
             _cootsTypes.AddRange(GameManager.Instance.CootsTypes);
-            _spriteRenderer.sprite = _cootsTypes[_typeIdx].sprite;
+            bottomUpHeadshot.sprite = _cootsTypes[_typeIdx].sprite;
+            bottomDownHeadshot.sprite = _cootsTypes[_typeIdx].sprite;
             
             CootsTypeChanged?.Invoke(_cootsTypes[_typeIdx]);
+        }
+
+        private bool HandleMouseDown()
+        {
+            if (!Input.GetMouseButtonDown(0) || bottomDown.activeSelf) return false;
+            var worldPos = MouseHelpers.MouseWorldPosition();
+            if (!_boxCollider2D.bounds.Contains(new Vector3(worldPos.x, worldPos.y, 0))) return false;
+            _buttonRenderer.sprite = onClickSprite;
+            bottomUp.gameObject.SetActive(false);
+            bottomDown.gameObject.SetActive(true);
+            return true;
         }
         
         private void Update()
         {
+            if (HandleMouseDown()) return;
+            
             if (!Input.GetMouseButtonUp(0)) return;
-
+            bottomUp.gameObject.SetActive(true);
+            bottomDown.gameObject.SetActive(false);
+            _buttonRenderer.sprite = baseSprite;
             var worldPos = MouseHelpers.MouseWorldPosition();
 
             if (!_boxCollider2D.bounds.Contains(new Vector3(worldPos.x, worldPos.y, 0))) return;
@@ -40,9 +62,11 @@ namespace SadBrains
             }
 
             var cootsType = _cootsTypes[_typeIdx];
-            _spriteRenderer.sprite = cootsType.sprite;
+            bottomUpHeadshot.sprite = _cootsTypes[_typeIdx].sprite;
+            bottomDownHeadshot.sprite = _cootsTypes[_typeIdx].sprite;
             CootsTypeChanged?.Invoke(cootsType);
         }
+        
 
     }
 }
